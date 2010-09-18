172a7249952ba3912e1f56f4922f50f859a74d5d
s1kevin <s1kevin@8b2bf132-9616-48f6-bddd-adfbbf42b433>
2010-09-18 00:00:17 +0000

    Improved source checking

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-testfile="Tests/AllTests.php"
-/usr/local/bin/phpunit --syntax-check $testfile && phpunit $testfile
-exit $?
 
+testfile="Tests/AllTests.php"
+for i in `find . -name \*.php -print`
+do
+    err="`php -l "$i" | grep -v \"^No syntax errors detected in $i\$\"`"
+    if [ ! -z "$err" ] ; then
+        echo "Syntax error detected in $i: $err"
+        exit 1
+    fi
+done
+echo "Code appears to be free from syntax errors."
+
+/usr/local/bin/phpunit $testfile
+exit $?
