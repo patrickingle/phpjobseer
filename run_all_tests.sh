ef88ff0dd0ac5d39c86bff5bce857a7c38e461c6
s1kevin <s1kevin@8b2bf132-9616-48f6-bddd-adfbbf42b433>
2011-02-10 01:25:34 +0000

    Test change to run_all_tests.sh

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -17,8 +17,10 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 # 
+
 testfile="Tests/AllTests.php"
 verbose=""
+
 if [ "--verbose" = "$1" ] ; then
     verbose="$1"
 fi
