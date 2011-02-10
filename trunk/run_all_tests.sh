79fdfa75842cd3503118436555bd14a381c7e4ee
s1kevin <s1kevin@8b2bf132-9616-48f6-bddd-adfbbf42b433>
2011-02-10 01:26:37 +0000

    Revert test change

--- trunk/run_all_tests.sh
+++ trunk/run_all_tests.sh
@@ -17,10 +17,8 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 # 
-
 testfile="Tests/AllTests.php"
 verbose=""
-
 if [ "--verbose" = "$1" ] ; then
     verbose="$1"
 fi
